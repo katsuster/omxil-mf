c40d43d473b7ba71abe872e39b8cd63d9c673825
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2015-03-14 17:34:53 +0900

    add debug interfaces.

---
+++ src/debug/dbgprint.c
@@ -0,0 +1,7 @@
+
+#include "debug/dbgprint.h"
+
+int no_printf(const char *fmt, ...)
+{
+	return 0;
+}
---
+++ src/debug/dbgprint.h
@@ -0,0 +1,36 @@
+
+#ifndef DBGPRINT_H__
+#define DBGPRINT_H__
+
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+
+#define gettid()    ((pid_t)syscall(SYS_gettid))
+
+#define errprint_cont(fmt, ...)    \
+	do { \
+		fprintf(stderr, fmt, ##__VA_ARGS__); \
+		fflush(stderr); \
+	} while (0)
+
+#if 1 //#ifdef ENABLE_DEBUG
+#define dprint_cont(fmt, ...)    errprint_cont(fmt, ##__VA_ARGS__)
+#else
+#define dprint_cont(fmt, ...)    no_printf(fmt, ##__VA_ARGS__)
+int no_printf(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
+#endif
+
+#ifdef __GNUC__
+#define DPRINT_FUNC    __func__
+//#define DPRINT_FUNC    __PRETTY_FUNCTION__
+#else
+#define DPRINT_FUNC    __func__
+#endif
+
+#define dprint_in(fmt, ...)     dprint_cont("[% 5d] in  %s:%d: " fmt, (int)gettid(), DPRINT_FUNC, __LINE__, ##__VA_ARGS__)
+#define dprint_out(fmt, ...)    dprint_cont("[% 5d] out %s:%d: " fmt, (int)gettid(), DPRINT_FUNC, __LINE__, ##__VA_ARGS__)
+#define dprint(fmt, ...)        dprint_cont("[% 5d]     %s:%d: " fmt, (int)gettid(), DPRINT_FUNC, __LINE__, ##__VA_ARGS__)
+#define errprint(fmt, ...)    errprint_cont("[% 5d]     %s:%d: " fmt, (int)gettid(), DPRINT_FUNC, __LINE__, ##__VA_ARGS__)
+
+#endif //DBGPRINT_H__
