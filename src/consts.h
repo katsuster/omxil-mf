0c5345676729ebb304048e49523f104e323a4b33
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2015-03-16 15:08:15 +0900

    add supported API version.

--- src/consts.h
+++ src/consts.h
@@ -2,4 +2,9 @@
 #ifndef CONSTS_HPP__
 #define CONSTS_HPP__
 
+#define OMX_MF_IL_MAJOR       1
+#define OMX_MF_IL_MINOR       1
+#define OMX_MF_IL_REVISION    2
+#define OMX_MF_IL_STEP        0
+
 #endif //CONSTS_HPP__
