df809e5788e8beddc0a3f918290b26077ea228e9
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2015-03-16 15:05:26 +0900

    add utility class.

---
+++ src/util/util.cpp
@@ -0,0 +1,40 @@
+#include "util/util.hpp"
+
+namespace mf {
+
+/**
+ * do 4bytes-swap.
+ *
+ * v      : 0x00010203
+ * return : 0x03020100
+ *
+ * @param v unsigned value
+ * @return swapped value
+ */
+uint32_t rev32(uint32_t v) {
+#ifdef __arm__
+	asm volatile ("rev %0, %1\n\t" : "=r"(v) : "r"(v));
+#else
+	v = ((v & 0x00ff00ff) <<  8) | ((v & 0xff00ff00) >>  8);
+	v = ((v & 0x0000ffff) << 16) | ((v & 0xffff0000) >> 16);
+#endif
+
+	return v;
+}
+
+/**
+ * do 2bytes-swap.
+ *
+ * v      : 0x0001
+ * return : 0x0100
+ *
+ * @param v unsigned value
+ * @return swapped value
+ */
+uint16_t rev16(uint16_t v) {
+	v = ((v & 0x00ff) <<  8) | ((v & 0xff00) >>  8);
+
+	return v;
+}
+
+} //namespace mf
---
+++ src/util/util.hpp
@@ -0,0 +1,34 @@
+#ifndef UTIL_HPP__
+#define UTIL_HPP__
+
+#include <cstdint>
+
+#include "debug/dbgprint.h"
+
+namespace mf {
+
+/**
+ * do 4bytes-swap.
+ *
+ * v      : 0x00010203
+ * return : 0x03020100
+ *
+ * @param v unsigned value
+ * @return swapped value
+ */
+uint32_t rev32(uint32_t v);
+
+/**
+ * do 2bytes-swap.
+ *
+ * v      : 0x0001
+ * return : 0x0100
+ *
+ * @param v unsigned value
+ * @return swapped value
+ */
+uint16_t rev16(uint16_t v);
+
+} //namespace mf
+
+#endif //UTIL_HPP__
