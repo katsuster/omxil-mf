75b8355ac6ee73ed6e581dbb43f8d1cac85fc1db
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2015-03-29 05:58:27 +0900

    fix component name correctly.

--- component/empty/src/renderer_null/renderer_null.cpp
+++ component/empty/src/renderer_null/renderer_null.cpp
@@ -1,14 +1,14 @@
 
-#include "render_null.hpp"
+#include "renderer_null/renderer_null.hpp"
 
 namespace mf {
 
-reader_null::reader_null(OMX_COMPONENTTYPE *c, OMX_STRING cname) 
+renderer_null::renderer_null(OMX_COMPONENTTYPE *c, OMX_STRING cname) 
 	: component(c, cname) {
 	//do nothing
 }
 
-reader_null::~reader_null() {
+renderer_null::~renderer_null() {
 	//do nothing
 }
 
--- component/empty/src/renderer_null/renderer_null.hpp
+++ component/empty/src/renderer_null/renderer_null.hpp
@@ -1,21 +1,22 @@
 
-#ifndef OMX_MF_COMPONENT_EMPTY__RENDER_NULL_HPP__
-#define OMX_MF_COMPONENT_EMPTY__RENDER_NULL_HPP__
+#ifndef OMX_MF_COMPONENT_EMPTY__RENDERER_NULL_HPP__
+#define OMX_MF_COMPONENT_EMPTY__RENDERER_NULL_HPP__
 
 #include <omxil_mf/omxil_mf.h>
 #include <omxil_mf/component.hpp>
 
 namespace mf {
 
-class reader_null : public component {
+class renderer_null : public component {
 public:
 	//親クラス
 	typedef component super;
 
-	reader_null(OMX_COMPONENTTYPE *c, OMX_STRING cname);
-	virtual ~reader_null();
+	renderer_null(OMX_COMPONENTTYPE *c, OMX_STRING cname);
+	virtual ~renderer_null();
 };
 
 } //namespace mf
 
-#endif //OMX_MF_COMPONENT_EMPTY__RENDER_NULL_HPP__
+#endif //OMX_MF_COMPONENT_EMPTY__RENDERER_NULL_HPP__
+
