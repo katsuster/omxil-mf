5edbca8d407957603c45ff62ba4da95acb4c505a
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2015-03-31 04:03:07 +0900

    add tests about GetHandle() API.

---
+++ tests/basic/get_handle.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -xe
+
+TEST_NAME=get_handle
+
+#./${TEST_NAME} OMX.st.video_decoder.avc
+#./${TEST_NAME} OMX.st.video_decoder.mpeg4
+#./${TEST_NAME} OMX.st.video_decoder.h263
+#./${TEST_NAME} OMX.st.audio_decoder.aac
+#./${TEST_NAME} OMX.st.audio_decoder.mp3
+#./${TEST_NAME} OMX.st.audio_decoder.vorbis
+#./${TEST_NAME} OMX.MF.reader.zero
+./${TEST_NAME} OMX.MF.renderer.null
