453a04621ab7fa3a1574ba64102f6d34df1914c8
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2015-03-31 04:04:01 +0900

    fix mistakes commit 119.

--- include/omxil_mf/omxil_mf.h
+++ include/omxil_mf/omxil_mf.h
@@ -9,17 +9,33 @@
 extern "C" {
 #endif
 
+#define OMX_MF_ENTRY_FUNCNAME    "OMX_MF_LibEntry"
+typedef OMX_ERRORTYPE OMX_APIENTRY (*OMX_MF_ENTRY_FUNC)(void);
+
 /**
  * The entry function of addtional components library.
  *
  * All components must implements this function.
+ *
+ * @return OMX_ErrorNone if success, OMX error value if failed.
  */
 OMX_API OMX_ERRORTYPE OMX_APIENTRY OMX_MF_LibEntry(void);
 
-#define OMX_MF_ENTRY_FUNCNAME    "OMX_MF_LibEntry"
-
-typedef OMX_ERRORTYPE OMX_APIENTRY (*OMX_MF_ENTRY_FUNC)(void);
+/**
+ * Constructor method of component.
+ * 
+ * @param cComponent: Pointer of OMX_COMPONENTTYPE allocated by OpenMAX MF lib.
+ * @param name      : Name of component. ex: "OMX.CompanyName.XXX.YYY"
+ * @return Casted (void *) pointer component class or inherited class instance, 
+ * nullptr if failed.
+ */
 typedef void *OMX_APIENTRY (*OMX_MF_CONSTRUCTOR_FUNC)(OMX_COMPONENTTYPE *cComponent, const char *name);
+
+/**
+ * Destructor method of component.
+ * 
+ * @param cComponent: Pointer of OMX_COMPONENTTYPE allocated by OpenMAX MF lib.
+ */
 typedef void OMX_APIENTRY (*OMX_MF_DESTRUCTOR_FUNC)(OMX_COMPONENTTYPE *cComponent);
 
 
