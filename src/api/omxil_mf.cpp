88c1c6a56fbccb35eda5cbdd7b1ce42508243e7c
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2015-04-24 15:13:24 +0900

    enable to add alias name of components.

--- src/api/omxil_mf.cpp
+++ src/api/omxil_mf.cpp
@@ -19,7 +19,7 @@ OMX_API OMX_ERRORTYPE OMX_APIENTRY OMX_MF_RegisterComponent(const char *name, co
 
 	res = rc->insert(name, comp_info);
 	if (!res) {
-		//already existed
+		//Component has already existed
 		return OMX_ErrorInvalidComponentName;
 	}
 
@@ -32,8 +32,28 @@ OMX_API OMX_ERRORTYPE OMX_APIENTRY OMX_MF_RegisterComponent(const char *name, co
 OMX_API OMX_ERRORTYPE OMX_APIENTRY OMX_MF_RegisterComponentAlias(const char *name, const char *alias)
 {
 	scoped_log_begin;
-	return OMX_ErrorNotImplemented;
-	//return OMX_ErrorNone;
+	mf::register_component *rc = mf::register_component::get_instance();
+	mf::register_info *rinfo = nullptr;
+	const OMX_MF_COMPONENT_INFO *comp_info = nullptr;
+	bool res;
+
+	rinfo = rc->find(name);
+	if (rinfo == nullptr) {
+		//not found
+		return OMX_ErrorInvalidComponentName;
+	}
+
+	comp_info = rinfo->comp_info;
+	res = rc->insert(alias, comp_info);
+	if (!res) {
+		//Alias has already existed
+		return OMX_ErrorInvalidComponentName;
+	}
+
+	//dump all registered components
+	rc->dump();
+
+	return OMX_ErrorNone;
 }
 
 OMX_API OMX_ERRORTYPE OMX_APIENTRY OMX_MF_RegisterComponentRole(const char *name, const char *role)
