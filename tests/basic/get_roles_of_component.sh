d82a63b6d4f47938e4abd982cf8fe26859f2442a
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2015-07-21 23:37:14 +0900

    Fix target component of OMX_GetRolesOfComponent test.

--- tests/basic/get_roles_of_component.sh
+++ tests/basic/get_roles_of_component.sh
@@ -4,9 +4,11 @@ set -xe
 
 TEST_NAME=get_roles_of_component
 
-./${TEST_NAME} OMX.st.video_decoder.avc
-./${TEST_NAME} OMX.st.video_decoder.mpeg4
+#./${TEST_NAME} OMX.st.video_decoder.avc
+#./${TEST_NAME} OMX.st.video_decoder.mpeg4
 #./${TEST_NAME} OMX.st.video_decoder.h263
-./${TEST_NAME} OMX.st.audio_decoder.aac
-./${TEST_NAME} OMX.st.audio_decoder.mp3
+#./${TEST_NAME} OMX.st.audio_decoder.aac
+#./${TEST_NAME} OMX.st.audio_decoder.mp3
 #./${TEST_NAME} OMX.st.audio_decoder.vorbis
+#./${TEST_NAME} OMX.MF.reader.zero
+./${TEST_NAME} OMX.MF.renderer.null
