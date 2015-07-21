7bddb7116bd730e162b6e6f368a9c14c6cb7bb62
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2015-07-21 09:49:49 +0900

    Wait for status changing in test of SendCommand.

--- tests/basic/test_send_command.cpp
+++ tests/basic/test_send_command.cpp
@@ -1,6 +1,10 @@
 
 #include <cstdio>
 #include <cstring>
+#include <mutex>
+#include <condition_variable>
+
+#include <unistd.h>
 
 #include <OMX_Core.h>
 #include <OMX_Component.h>
@@ -15,7 +19,7 @@ extern "C" {
 class comp_test_send_cmd : public omxil_comp {
 public:
 	comp_test_send_cmd(const char *comp_name)
-		: omxil_comp(comp_name)
+		: omxil_comp(comp_name), event_done(false)
 	{
 		//do nothing
 	}
@@ -27,15 +31,39 @@ public:
 
 	virtual OMX_ERRORTYPE EventHandler(OMX_HANDLETYPE hComponent, OMX_PTR pAppData, OMX_EVENTTYPE eEvent, OMX_U32 nData1, OMX_U32 nData2, OMX_PTR pEventData)
 	{
-		printf("!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+		if (eEvent == OMX_EventCmdComplete && 
+			nData1 == OMX_CommandStateSet && 
+			nData2 == OMX_StateIdle) {
+			std::unique_lock<std::mutex> lock(mut_command);
+
+			printf("comp_test_send_cmd::EventHandler idle state.\n");
+			event_done = true;
+			cond_command.notify_all();
+		} else {
+			printf("comp_test_send_cmd::EventHandler ignored.\n");
+		}
+
+		return OMX_ErrorNone;
 	}
+
+	void wait_event()
+	{
+		std::unique_lock<std::mutex> lock(mut_command);
+
+		cond_command.wait(lock, [&] { return event_done; });
+	}
+
+private:
+	std::mutex mut_command;
+	std::condition_variable cond_command;
+	bool event_done;
 
 };
 
 int main(int argc, char *argv[])
 {
 	const char *arg_comp;
-	omxil_comp *comp;
+	comp_test_send_cmd *comp;
 	OMX_PORT_PARAM_TYPE parm_v, parm_a;
 	OMX_BUFFERHEADERTYPE *buf_in, *buf_out;
 	OMX_ERRORTYPE result;
@@ -101,15 +129,14 @@ int main(int argc, char *argv[])
 	printf("OMX_GetParameter: audio -----\n");
 	dump_port_param_type(&parm_a);
 
-
 	/*
 	OMX_VIDEO_PARAM_AVCTYPE
 	result = OMX_SetParameter(comp, OMX_IndexParamVideoAvc, );
 	if (result != OMX_ErrorNone) {
 		fprintf(stderr, "OMX_SetParameter() failed.\n");
 		goto err_out3;
-	}*/
-
+	}
+	*/
 
 	result = comp->SendCommand(OMX_CommandStateSet, 
 		OMX_StateIdle, 0);
@@ -135,6 +162,9 @@ int main(int argc, char *argv[])
 	}
 	printf("OMX_AllocateBuffer: \n");
 	dump_port_bufferheadertype(buf_out);
+
+	//Wait for StatusIdle
+	comp->wait_event();
 
 	delete comp;
 
