db8012c4acfc4789a4dd10f370387a9f389165a2
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2015-07-30 00:52:27 +0900

    Add EmptyThisBuffer and FillThisBuffer combination test.

--- component/empty/src/filter_copy/filter_copy.cpp
+++ component/empty/src/filter_copy/filter_copy.cpp
@@ -8,7 +8,7 @@ filter_copy::filter_copy(OMX_COMPONENTTYPE *c, const char *cname)
 	in_port_video(nullptr), out_port_video(nullptr)
 {
 	try {
-		in_port_video = new port_video(0, this);
+		in_port_video = new port_video(4, this);
 		in_port_video->set_dir(OMX_DirInput);
 		in_port_video->set_buffer_count_actual(4);
 		in_port_video->set_buffer_count_min(1);
@@ -19,9 +19,9 @@ filter_copy::filter_copy(OMX_COMPONENTTYPE *c, const char *cname)
 
 		insert_port(*in_port_video);
 
-		out_port_video = new port_video(1, this);
+		out_port_video = new port_video(5, this);
 		out_port_video->set_dir(OMX_DirOutput);
-		out_port_video->set_buffer_count_actual(4);
+		out_port_video->set_buffer_count_actual(3);
 		out_port_video->set_buffer_count_min(1);
 		out_port_video->set_buffer_size(65536);
 		out_port_video->set_buffer_alignment(64);
@@ -49,7 +49,51 @@ filter_copy::~filter_copy()
 
 void filter_copy::run()
 {
-	//do nothing
+	OMX_ERRORTYPE result;
+	port_buffer pb_in, pb_out;
+	OMX_U32 off_in, off_out, len_in, len_out;
+	OMX_TICKS stamp = 0;
+	int i = 0;
+
+	while (should_run()) {
+		result = in_port_video->pop_buffer(&pb_in);
+		if (result != OMX_ErrorNone) {
+			errprint("in_port_video.pop_buffer().\n");
+			break;
+		}
+
+		result = out_port_video->pop_buffer(&pb_out);
+		if (result != OMX_ErrorNone) {
+			errprint("out_port_video.pop_buffer().\n");
+			break;
+		}
+
+		//memset(pb_out.header->pBuffer, 0, pb_out.header->nAllocLen);
+
+		off_in = pb_in.header->nOffset;
+		len_in = pb_in.header->nFilledLen;
+		off_out = pb_out.header->nOffset;
+		len_out = pb_out.header->nAllocLen - off_out;
+
+		len_out = std::min(len_in, len_out);
+		memmove(&pb_out.header->pBuffer[off_out], 
+			&pb_in.header->pBuffer[off_in], len_out);
+
+		//NOTE: gst-openmax ‚Í nOffset ‚ð–ß‚³‚È‚¢‚Æ‚¨‚©‚µ‚È‹““®‚ð‚·‚éHH
+		pb_in.header->nOffset = 0;
+		in_port_video->empty_buffer_done(&pb_in);
+
+		pb_out.header->nFilledLen = len_out;
+		pb_out.header->nOffset    = 0;
+		pb_out.header->nTimeStamp = stamp;
+		pb_out.header->nFlags     = 0;
+		out_port_video->fill_buffer_done(&pb_out);
+
+		//next one
+		i++;
+		//16ms
+		stamp += 16000;
+	}
 }
 
 
@@ -64,7 +108,7 @@ OMX_U32 filter_copy::get_video_ports()
 
 OMX_U32 filter_copy::get_video_start_port()
 {
-	return 0;
+	return 4;
 }
 
 
