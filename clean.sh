2feb8018832a68ae946de9db84a88f7dc98e1399
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2015-12-20 19:13:07 +0900

    Add ignore files.

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+find -name configure      | xargs rm -rf
+find -name Makefile       | xargs rm -rf
+find -name Makefile.in    | xargs rm -rf
+find -name config.h.in    | xargs rm -rf
+find -name aclocal.m4     | xargs rm -rf
+find -name autom4te.cache | xargs rm -rf
+find -name .deps          | xargs rm -rf
+
