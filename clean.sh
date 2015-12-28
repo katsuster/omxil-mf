9d8f89ed375bbdb9521b1235cb8aada128391a81
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2015-12-29 00:30:58 +0900

    Cleanup generated files by doxygen.

--- clean.sh
+++ clean.sh
@@ -8,4 +8,4 @@ find -name config.h.in~   | xargs rm -rf
 find -name aclocal.m4     | xargs rm -rf
 find -name autom4te.cache | xargs rm -rf
 find -name .deps          | xargs rm -rf
-
+find -name doxygen_sqlite3.db | xargs rm -rf
