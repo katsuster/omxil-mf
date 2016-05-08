d49a3560ac70ffef3c54432afa20e875dff508fe
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2016-05-08 11:16:02 +0900

    Cleanup configure.ac.

--- clean.sh
+++ clean.sh
@@ -2,12 +2,16 @@
 
 rm -rf conf
 rm -rf m4
-find -name configure      | xargs rm -rf
-find -name Makefile       | xargs rm -rf
-find -name Makefile.in    | xargs rm -rf
-find -name config.h.in    | xargs rm -rf
-find -name config.h.in~   | xargs rm -rf
-find -name aclocal.m4     | xargs rm -rf
-find -name autom4te.cache | xargs rm -rf
+find -name configure      | xargs rm -f
+find -name Makefile       | xargs rm -f
+find -name Makefile.in    | xargs rm -f
+find -name config.h.in    | xargs rm -f
+find -name config.h.in~   | xargs rm -f
+find -name aclocal.m4     | xargs rm -f
+find -name libtool.m4     | xargs rm -f
+find -name lt~obsolete.m4 | xargs rm -f
+find -name ltoptions.m4   | xargs rm -f
+find -name ltsugar.m4     | xargs rm -f
+find -name ltversion.m4   | xargs rm -f
 find -name .deps          | xargs rm -rf
 find -name doxygen_sqlite3.db | xargs rm -rf
