26f450374f0b94f9acbb10e9fe909831f0c940a7
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2016-05-08 11:37:25 +0900

    Fix indents.

--- component/reader_ts/src/entry/windll.cpp
+++ component/reader_ts/src/entry/windll.cpp
@@ -2,6 +2,8 @@
 
 /*
  * NOTE: Some macros must be defined in project options of Visual Studio.
+ *       - _CRT_SECURE_NO_WARNINGS
+ *         To use strncpy().
  *       - NOMINMAX
  *         To use std::min(), std::max().
  * NOTE: Suppress some warnings of Visual Studio.
@@ -10,19 +12,16 @@
 
 #include <windows.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
-	switch (ul_reason_for_call)
-	{
+	switch (ul_reason_for_call) {
 	case DLL_PROCESS_ATTACH:
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
 	case DLL_PROCESS_DETACH:
 		break;
 	}
+
 	return TRUE;
 }
 
--- src/api/windll.cpp
+++ src/api/windll.cpp
@@ -21,6 +21,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 	case DLL_PROCESS_DETACH:
 		break;
 	}
+
 	return TRUE;
 }
 
