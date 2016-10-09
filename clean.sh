850131e23a188f9320e33cabd7ca40ce071d4078
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2016-10-09 23:56:27 +0900

    Refine the cleanup script for auto generated files.

--- clean.sh
+++ clean.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-find -name conf -a -type d | xargs rm -rf
-find -name m4 -a -type d   | xargs rm -rf
-
 find -name configure      | xargs rm -f
 find -name Makefile       | xargs rm -f
 find -name Makefile.in    | xargs rm -f
@@ -14,6 +11,15 @@ find -name lt~obsolete.m4 | xargs rm -f
 find -name ltoptions.m4   | xargs rm -f
 find -name ltsugar.m4     | xargs rm -f
 find -name ltversion.m4   | xargs rm -f
+find -name ltmain.sh      | xargs rm -f
+find -name config.guess   | xargs rm -f
+find -name config.sub     | xargs rm -f
+find -name compile     -a -executable | xargs rm -f
+find -name depcomp     -a -executable | xargs rm -f
+find -name install-sh  -a -executable | xargs rm -f
+find -name missing     -a -executable | xargs rm -f
+find -name test-driver -a -executable | xargs rm -f
+
 find -name autom4te.cache | xargs rm -rf
 find -name .deps          | xargs rm -rf
 find -name doxygen_sqlite3.db | xargs rm -rf
